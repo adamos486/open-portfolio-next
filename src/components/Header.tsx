02e7ae3d4b7e2e3e54cf1db845cd5469eeb76815
Adam Cobb <adamos486@gmail.com>
2025-02-24 20:54:50 -0800

    Adding a header component and experimenting with Daisy UI components.

---
+++ src/components/Header.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Header: React.FC = () => {
+  return (
+    <header className="navbar bg-base-100 shadow-md px-4 py-2">
+      <div className="flex-1">
+        <a className="btn btn-ghost normal-case text-xl">Home</a>
+      </div>
+
+    </header>
+  )
+}
+
+export default Header;
