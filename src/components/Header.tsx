18ad214c7c51c88e29224ce5fb8d6b83c49bbf22
Adam Cobb <adamos486@gmail.com>
2025-02-24 21:10:38 -0800

    Adding first menu button, linking up blog page.

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -7,7 +7,13 @@ const Header: React.FC = () => {
       <div className="flex-1">
         <Link className="btn btn-ghost normal-case text-xl" href="/">Home</Link>
       </div>
-
+      <div className="flex-none">
+        <ul className="menu menu-horizontal p-0">
+          <li>
+            <Link className="px-4 py-2 hover:text-primary" href="/blog">Blog</Link>
+          </li>
+        </ul>
+      </div>
     </header>
   )
 }
---
+++ src/pages/blog.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Blog: React.FC = () => {
+  return (
+    <h1>Substack here.</h1>
+  );
+};
+
+export default Blog;
