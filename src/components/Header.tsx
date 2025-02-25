2edfaa1237c4fc76f67d0138ece6322e1b5b3208
Adam Cobb <adamos486@gmail.com>
2025-02-24 21:03:01 -0800

    Better styling of Header.tsx

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,10 +1,11 @@
 import React from "react";
+import Link from "next/link";
 
 const Header: React.FC = () => {
   return (
-    <header className="navbar bg-base-100 shadow-md px-4 py-2">
+    <header className="navbar bg-base-300 shadow-md px-4 py-2">
       <div className="flex-1">
-        <a className="btn btn-ghost normal-case text-xl">Home</a>
+        <Link className="btn btn-ghost normal-case text-xl" href="/">Home</Link>
       </div>
 
     </header>
