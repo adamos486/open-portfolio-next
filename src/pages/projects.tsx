4a8fdc37bebc7c6bbf6d990f81edc67af7d04c9d
Adam Cobb <adamos486@gmail.com>
2025-02-25 19:37:35 -0800

    Blog and Projects now confirm to the master site style.

--- src/pages/projects.tsx
+++ src/pages/projects.tsx
@@ -1,9 +1,14 @@
 import React from "react";
+import Header from "@/components/Header";
+import "../app/globals.css";
 
 const Projects: React.FC = () => {
   return (
-    <h1>Links of project posts here.</h1>
+    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+      <Header />
+      <h1>Links of project posts here.</h1>
+    </div>
   );
-};
+}
 
 export default Projects;
