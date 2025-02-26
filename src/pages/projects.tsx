0f1a3b425f5cf0e20ea645b3fc1c30fff402f7b2
Adam Cobb <adamos486@gmail.com>
2025-02-25 19:48:29 -0800

    Creating Template.tsx where I can reuse several components across all main pages.

--- src/pages/projects.tsx
+++ src/pages/projects.tsx
@@ -1,13 +1,12 @@
 import React from "react";
 import Header from "@/components/Header";
-import "../app/globals.css";
+import Template from "@/components/Template";
 
 const Projects: React.FC = () => {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <Header />
+    <Template>
       <h1>Links of project posts here.</h1>
-    </div>
+    </Template>
   );
 }
 
