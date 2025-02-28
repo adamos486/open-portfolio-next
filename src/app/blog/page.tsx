a32214329a55114c0a4bbdd2000437f19dc8f9f0
Adam Cobb <adamos486@gmail.com>
2025-02-27 21:55:34 -0800

    First feed calls to substack.

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,6 +1,21 @@
 import React from "react";
+import Parser from "rss-parser";
 
-const Blog: React.FC = () => {
+type FeedItem = {
+  title: string
+  link: string;
+  pubDate: string;
+  contentSnippet?: string;
+};
+
+// type BlogPageProps = {
+//   posts: FeedItem[];
+// };
+
+const Blog: React.FC = async () => {
+  const parser = new Parser();
+  const feed = await parser.parseURL("https://strikingbalance.substack.com/feed");
+  console.log({feed});
   return (
     <h1>Substack here.</h1>
   );
